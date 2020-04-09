45399bef9e04cb020bab3c9c423bf5ea203e4fd5
trentfridey <tfri.92@gmail.com>
2020-04-08 23:44:58 -0500

    update lib with boundary condition logic

--- src/lib.rs
+++ src/lib.rs
@@ -6,13 +6,6 @@ use std::f32;
 use std::f32::consts::PI;
 use wasm_bindgen::prelude::*;
 
-extern crate web_sys;
-macro_rules! log {
-     ($($t:tt)*) => {
-         web_sys::console::log_1(&format!($($t)*).into());
-     };
- }
-
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
 #[cfg(feature = "wee_alloc")]
@@ -27,6 +20,7 @@ pub struct Laboratory {
     image: Vec<u32>,
 }
 
+#[no_mangle]
 pub fn complex_to_rgba(complex: Complex<f32>) -> u32 {
     // takes psi at an index and computes
     // rgba value via norm -> value
@@ -87,55 +81,52 @@ impl Laboratory {
         (row * self.width + col) as usize
     }
 
-    fn is_valid_index(&self, row: u32, col: u32) -> bool {
-        (row < self.height) && (col < self.width)
-    }
-
     fn psi_evolve(&self, row: u32, col: u32) -> Complex<f32> {
         // finite difference algorithm
         // computes psi_next a the point specified by row, col
+        // using a spatial stencil:
+        // [[0,1,0],[1,-4,1],[0,1,0]]
+        // We also implement Neumann boundary conditions by modifiying the
+        // stencil to enable reflections. For example, on the left wall the
+        // stencil becomes: [[0,1,0],[0,-4,2],[0,1,0]]
         let mut psi_next = Complex { im: 0.0, re: 0.0 };
         let idx = self.get_index(row, col);
-        // here we must implement the boundary conditions
-        // if neighbor is out of bounds, it does not contribute to the center
-        // yet a hard wall boundary means the spatial derivative must also vanish
-        // which means the wave should reflect back
         let psi_ctr = self.psi[idx];
-        let neighbors = if row != 0 && col != 0 {
-            vec![
-                (row - 1, col),
-                (row, col - 1),
-                (row + 1, col),
-                (row, col + 1),
-            ]
-        } else if row != 0 && col == 0 {
-            // wave has reached left wall, need to reflect to right
-            vec![(row - 1, col), (row + 1, col), (row, col + 1)]
-        } else if col != 0 && row == 0 {
-            // wave has reached top wall, need to reflect down
-            vec![(row, col - 1), (row + 1, col), (row, col + 1)]
-        } else {
-            // wave has reached top left corner, reflect down and right
-            vec![(row + 1, col), (row, col + 1)]
-        };
-        let psi_neighbors: Complex<f32> = neighbors
-            .into_iter()
-            .map(|n| {
-                if self.is_valid_index(n.0, n.1) {
-                    self.psi[self.get_index(n.0, n.1)]
-                } else {
-                    Complex { re: 0.0, im: 0.0 }
-                }
-            })
-            .fold(Complex { re: 0.0, im: 0.0 }, |acc, p| acc + p);
+        let psi_neighbors = if col == 0 && row != 0 && row != self.height { 
+            // left wall
+                self.psi[self.get_index(row - 1, col)]
+                + self.psi[self.get_index(row + 1, col)]
+                + 2.0 * self.psi[self.get_index(row, col + 1)]
+            } else if col == self.width && row != 0 && row != self.height {
+                // right wall
+                   self.psi[self.get_index(row - 1, col)]
+                   + 2.0 * self.psi[self.get_index(row, col - 1)]
+                   + self.psi[self.get_index(row + 1, col)]
+            } else if row == 0 && col != 0 && col != self.width {
+                // top wall
+                    self.psi[self.get_index(row, col - 1)]
+                    + 2.0 * self.psi[self.get_index(row + 1, col)]
+                    + self.psi[self.get_index(row, col + 1)]
+            } else if row == self.height && col != 0 && col != self.width {
+                // bottom wall
+                   2.0 * self.psi[self.get_index(row - 1, col)]
+                   + self.psi[self.get_index(row, col - 1)]
+                   + self.psi[self.get_index(row, col + 1)]
+            } else {
+                // default
+                    self.psi[self.get_index(row - 1, col)]
+                    + self.psi[self.get_index(row, col - 1)]
+                    + self.psi[self.get_index(row + 1, col)]
+                    + self.psi[self.get_index(row, col + 1)]
+            };
         psi_next.re = -1.0 * (psi_neighbors.im - 4.0 * psi_ctr.im - psi_ctr.re);
         psi_next.im = psi_neighbors.re - 4.0 * psi_ctr.re - psi_ctr.im;
         return psi_next;
     }
 }
-
 #[wasm_bindgen]
 impl Laboratory {
+    #[no_mangle]
     pub fn new() -> Laboratory {
         let height: u32 = 200;
         let width: u32 = 200;
@@ -161,18 +152,19 @@ impl Laboratory {
         }
     }
 
+    #[no_mangle]
     pub fn step(&mut self) {
         let mut next = self.psi.clone();
         // normalize the result - if any cells have a non-zero value, count
-        let mut count = 0.0;
+        // let mut count = 0.0;
 
         for row in 0..self.height {
             for col in 0..self.width {
                 let idx = self.get_index(row, col);
                 next[idx] = self.psi_evolve(row, col);
-                if next[idx].norm() > 0.0 {
-                    count += 1.0
-                };
+                // if next[idx].norm() > 0.0 {
+                //     count += 1.0
+                // };
             }
         }
         let next_psi: Vec<Complex<f32>> = next.into_iter().map(|c| c).collect();
@@ -180,12 +172,15 @@ impl Laboratory {
         self.psi = next_psi;
     }
 
+    #[no_mangle]
     pub fn image(&self) -> *const u32 {
         self.image.as_ptr()
     }
+    #[no_mangle]
     pub fn width(&self) -> u32 {
         self.width
     }
+    #[no_mangle]
     pub fn height(&self) -> u32 {
         self.height
     }
