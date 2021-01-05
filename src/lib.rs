374f9bd5e147f4ae871e7ddf5aaff690b623f74f
trentfridey <tfri.92@gmail.com>
2021-01-04 22:01:51 -0500

    refactor to use enums and traits

--- src/lib.rs
+++ src/lib.rs
@@ -11,10 +11,11 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 const ALPHA: u32 = 0xFF000000;
 
-const DEFAULT: u8 = 0;
-const WALL: u8 = 1;
-const PTRANSMITTER: u8 = 2;
-const NTRANSMITTER: u8 = 3;
+#[derive(Clone)]
+enum Status {
+    Default,
+    Wall,
+}
 const FORCE_DAMPING_BIT_SHIFT: u8 = 4;
 
 #[wasm_bindgen]
@@ -25,10 +26,14 @@ pub struct Arena {
     v: Vec<i32>,
     force: Vec<i32>,
     image: Vec<u32>,
-    status: Vec<u8>,
+    status: Vec<Status>,
 }
 
-pub fn applyCap(x: i32) -> i32 {
+trait HexColor {
+    fn to_rgb(self) -> u32;
+}
+
+pub fn apply_cap(x: i32) -> i32 {
     if x < i32::MIN >> 1 {
         return i32::MIN >> 1;
     } else if x > i32::MAX >> 1 {
@@ -38,14 +43,16 @@ pub fn applyCap(x: i32) -> i32 {
     }
 }
 
-pub fn toRGB(x: i32) -> u32 {
-    let mut val: i32 = x >> 22;
-    if val > 0 {
-        let res = val as u32;
-        return ((res << 8) | (res << 16) | ALPHA);
-    } else {
-        val = std::cmp::max(val, -255i32);
-        return (-1i32 * val) as u32 | ALPHA;
+impl HexColor for i32 {
+    fn to_rgb(self) -> u32 {
+        let mut val: i32 = self >> 22;
+        if val > 0 {
+            let res = val as u32;
+            return (res << 8) | (res << 16) | ALPHA;
+        } else {
+            val = std::cmp::max(val, -255_i32);
+            return (-1_i32 * val) as u32 | ALPHA;
+        }
     }
 }
 
@@ -58,31 +65,33 @@ impl Arena {
         let height: u32 = 200;
         let w: usize = width as usize;
         let h: usize = height as usize;
-        let mut status = vec![DEFAULT; w * h]; 
-        let mut u_0: Vec<i32> = vec![0; w*h]; 
+        let mut status = vec![Status::Default; w * h];
+        let mut u_0: Vec<i32> = vec![0; w * h];
         // Draw walls along the outer boundary
+        // left and right walls
         for i in 0..h {
-            status[i * w] = WALL;
-            status[i * w + w - 1] = WALL;
+            status[i * w] = Status::Wall;
+            status[i * w + w - 1] = Status::Wall;
         }
+        // top and bottom walls
         for j in 0..w {
-            status[j] = WALL;
-            status[w * h - w + j] = WALL;
+            status[j] = Status::Wall;
+            status[w * h - w + j] = Status::Wall;
         }
 
+        // initial conditions
         for i in 0..w {
             for j in 0..h {
                 if i > 50 && j > 50 && i < 100 && j < 100 {
-                    u_0[j*w + i] = 0x3fffffff
+                    u_0[j * w + i] = 0x3fffffff
                 }
             }
         }
-            
-       
-        let v_0: Vec<i32> = vec![0; w*h]; 
-        let force_0: Vec<i32> = vec![0; w*h];
 
-        let image_0: Vec<u32> = u_0.iter().map(|&x| {toRGB(applyCap(x))}).collect(); 
+        let v_0: Vec<i32> = vec![0; w * h];
+        let force_0: Vec<i32> = vec![0; w * h];
+
+        let image_0: Vec<u32> = u_0.iter().map(|&x| apply_cap(x).to_rgb()).collect();
         return Arena {
             height,
             width,
@@ -95,52 +104,41 @@ impl Arena {
     }
 
     #[no_mangle]
-    pub fn step(&mut self, signalAmplitude: i32, dampingBitShift: u8) {
-        // First loop: look for noise generator pixels and set their values in u:
+    pub fn step(&mut self,  damping_bit_shift: u8) {
         let w = self.width as usize;
         let h = self.height as usize;
-        for i in (0..w * h) {
-            if self.status[i] == PTRANSMITTER {
-                self.u[i] = signalAmplitude;
-                self.v[i] = 0;
-                self.force[i] = 0;
-            }
-            if self.status[i] == NTRANSMITTER {
-                self.u[i] = -signalAmplitude;
-                self.v[i] = 0;
-                self.force[i] = 0;
-            }
-        }
-        // Second loop: apply wave equation at all pixels
-        for i in (0..w * h) {
-            if self.status[i] == DEFAULT {
-                let uCen = self.u[i];
-                let uNorth = self.u[i - w];
-                let uSouth = self.u[i + w];
-                let uEast = self.u[i + 1];
-                let uWest = self.u[i - 1];
-                let uxx = ((uWest + uEast) >> 1) - uCen;
-                let uyy = ((uNorth + uSouth) >> 1) - uCen;
-                let mut vel = self.v[i] + (uxx >> 1) + (uyy >> 1);
-                if dampingBitShift > 0 {
-                    vel -= (vel >> dampingBitShift);
-                }
-                self.v[i] = applyCap(vel);
+        for i in 0..w * h {
+            match self.status[i] {
+                Status::Default => {
+                    let u_cen = self.u[i];
+                    let u_north = self.u[i - w];
+                    let u_south = self.u[i + w];
+                    let u_east = self.u[i + 1];
+                    let u_west = self.u[i - 1];
+                    let uxx = ((u_west + u_east) >> 1) - u_cen;
+                    let uyy = ((u_north + u_south) >> 1) - u_cen;
+                    let mut vel = self.v[i] + (uxx >> 1) + (uyy >> 1);
+                    if damping_bit_shift > 0 {
+                        vel -= vel >> damping_bit_shift;
+                    }
+                    self.v[i] = apply_cap(vel);
+                },
+                _ => {}
             }
         }
-
         // Apply forces from the mouse:
         for i in 0..w * h {
-            if self.status[i] == DEFAULT {
-                let mut f = self.force[i];
-                self.u[i] = applyCap(f + applyCap(self.u[i] + self.v[i]));
-                f -= (f >> FORCE_DAMPING_BIT_SHIFT);
-                self.force[i] = f;
-            }
-            if self.status[i] == WALL {
-                self.image[i] = 0x00000000;
-            } else {
-                self.image[i] = toRGB(self.u[i]);
+            match self.status[i] {
+                Status::Default => {
+                    let mut f = self.force[i];
+                    self.u[i] = apply_cap(f + apply_cap(self.u[i] + self.v[i]));
+                    f -= f >> FORCE_DAMPING_BIT_SHIFT;
+                    self.force[i] = f;
+                    self.image[i] = self.u[i].to_rgb();
+                },
+                Status::Wall => {
+                    self.image[i] = 0x00000000;
+                },
             }
         }
     }
