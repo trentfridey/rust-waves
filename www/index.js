2ca45cbccc45d6fbae36baa35f2ed18169ebfb00
trentfridey <tfri.92@gmail.com>
2021-01-04 22:02:29 -0500

    refactor to split libSchr logic from index logic

--- www/index.js
+++ www/index.js
@@ -1,51 +1,9 @@
 import { memory } from 'rust-waves/rust_waves_bg';
-// import { Laboratory } from 'rust-waves'; 
 import { Arena } from 'rust-waves';
 
-const runLab = () => {
- const lab = Laboratory.new();
- const width = lab.width();
- const height = lab.height();
-
-let canvas = document.getElementById('canvas');
-let ctx = canvas.getContext('2d');
-
-const drawPsi = () => {
-    const imagePtr = lab.image();
-    const imageArray = new Uint8ClampedArray(memory.buffer, imagePtr, 4 * height * width);
-    let imageData = new ImageData(imageArray, width, height);
-    ctx.putImageData(imageData, 0, 0);
-};
-
-let running = false;
-const render = ({loop = true, debug = false}) => {
-    lab.step();
-    drawPsi();
-    if(loop){
-        if(running) requestAnimationFrame(render);
-    }
-};
-
-let debug = document.getElementById('debug').checked;
-
-let intensity = document.getElementById('view').checked;
-
-let step = document.getElementById('step');
-step.addEventListener('click', () => {
-  render({debug});  
-});
-
-let start = document.getElementById('start');
-start.addEventListener('click', () => {
-    start.innerHTML = 'Stop';
-    running = !running;
-    render({loop: true, debug});
-});
-};
-
- const arena = Arena.new();
- const width = arena.width();
- const height = arena.height();
+const arena = Arena.new();
+const width = arena.width();
+const height = arena.height();
 
 let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
@@ -66,18 +24,18 @@ const windowToCanvas = (canvas, x, y) => {
         x: Math.round(x - bbox.left * (canvas.width / bbox.width)),
         y: Math.round(y - bbox.top * (canvas.height / bbox.height))
     };
-}
+};
 
 canvas.onmousedown = e => {
     e.preventDefault();
     const loc = windowToCanvas(canvas, e.clientX, e.clientY);
     const targetIndex = loc.x + loc.y * width;
     forceArray[targetIndex] = 0x3fffffff;
-}
+};
 
 let running = false;
 const render = ({loop = true, debug = false}) => {
-    arena.step(1,0);
+    arena.step(0);
     drawSound();
     if(loop){
         if(running) requestAnimationFrame(render);
