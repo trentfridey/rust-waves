1b037d749160c8c413f08afeb4e403c5d54491a9
trentfridey <tfri.92@gmail.com>
2021-05-23 15:53:30 -0400

    update index.js for restructured wasm imports

--- js/index.js
+++ js/index.js
@@ -1,59 +1,80 @@
-import { memory, Arena } from '../crate/Cargo.toml' // This might be incorrect
+import rust, { Canvas } from '../crate/Cargo.toml' 
 
-const arena = Arena.new();
-const width = arena.width();
-const height = arena.height();
-
+console.log(Canvas, rust.wasm.memory)
 let canvas = document.getElementById('canvas');
+const rustCanvas = Canvas.new(canvas.width, canvas.height);
+const width = rustCanvas.width();
+const height = rustCanvas.height();
 let ctx = canvas.getContext('2d');
 
-const drawSound = () => {
-    const imagePtr = arena.image();
-    const imageArray = new Uint8ClampedArray(memory.buffer, imagePtr, 4 * height * width);
+const debug = () => {
+    let debugCanvas = document.getElementById('test-coloring');
+    let debugCtx = debugCanvas.getContext('2d');
+    const testPtr = rustCanvas.test();
+    const testArray = new Uint8ClampedArray(rust.wasm.memory.buffer, testPtr, 4 * height * width);
+    let testData = new ImageData(testArray, width, height);
+    debugCtx.putImageData(testData, 0, 0);
+}
+
+debugBtn = document.getElementById('debug-btn')
+debugBtn.addEventListener('click', debug)
+
+
+// let intensity = document.getElementById('view').checked;
+
+// let step = document.getElementById('step');
+// step.addEventListener('click', () => {
+//   render({debug});  
+// });
+
+// canvas.onmousedown = e => {
+//     e.preventDefault();
+//     const loc = windowToCanvas(canvas, e.clientX, e.clientY);
+//     const targetIndex = loc.x + loc.y * width;
+//     forceArray[targetIndex] = 0x3fffffff;
+// };
+
+// const windowToCanvas = (canvas, x, y) => {
+//     const bbox = canvas.getBoundingClientRect();
+//     return {
+//         x: Math.round(x - bbox.left * (canvas.width / bbox.width)),
+//         y: Math.round(y - bbox.top * (canvas.height / bbox.height))
+//     };
+// };
+let t0 = 0;
+let t1 = 1;
+let duration = t0 - t1;
+let frameCount = 0
+
+const FPSCounter = document.getElementById('fps')
+
+let id = null;
+
+
+function render(){
+    id = null;
+    t0 = performance.now()
+    frameCount++
+
+    rustCanvas.step();
+    const imagePtr = rustCanvas.image();
+    const imageArray = new Uint8ClampedArray(rust.wasm.memory.buffer, imagePtr, 4 * height * width);
     let imageData = new ImageData(imageArray, width, height);
     ctx.putImageData(imageData, 0, 0);
+    t1 = performance.now()
+    duration = t1 - t0
+    run();
 };
 
-const forcePtr = arena.force();
-const forceArray = new Int32Array(memory.buffer, forcePtr, height * width);
-
-const windowToCanvas = (canvas, x, y) => {
-    const bbox = canvas.getBoundingClientRect();
-    return {
-        x: Math.round(x - bbox.left * (canvas.width / bbox.width)),
-        y: Math.round(y - bbox.top * (canvas.height / bbox.height))
-    };
-};
-
-canvas.onmousedown = e => {
-    e.preventDefault();
-    const loc = windowToCanvas(canvas, e.clientX, e.clientY);
-    const targetIndex = loc.x + loc.y * width;
-    forceArray[targetIndex] = 0x3fffffff;
-};
-
-let running = false;
-const render = ({loop = true, debug = false}) => {
-    arena.step(0);
-    drawSound();
-    if(loop){
-        if(running) requestAnimationFrame(render);
-    }
-};
-
-let debug = document.getElementById('debug').checked;
-
-let intensity = document.getElementById('view').checked;
-
-let step = document.getElementById('step');
-step.addEventListener('click', () => {
-  render({debug});  
-});
+function run (){
+    if(frameCount %10 === 0) FPSCounter.innerText = Math.floor(1e3/duration)
+    if (!id) { id = requestAnimationFrame(render) }
+}
 
 let start = document.getElementById('start');
+let running = false;
 start.addEventListener('click', () => {
     running = !running;
-    if(running) start.innerHTML = 'Stop';
-    else start.innerHTML = 'Start';
-    render({loop: true, debug});
+    if(running) { run(); start.innerHTML = 'Stop'; }
+    else { cancelAnimationFrame(id); id = null; start.innerHTML = 'Start';  }
 });
