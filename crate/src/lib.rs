fc4ef4fc7c6fdfbfedda4a72bf5537d9b93af1b1
trentfridey <tfri.92@gmail.com>
2021-07-20 23:16:52 -0400

    refactor; add norm-related code

--- crate/src/lib.rs
+++ crate/src/lib.rs
@@ -5,13 +5,14 @@ use wasm_bindgen::prelude::*;
 
 
 mod utils;
-use utils::{Arena, QWave, CWave, Waveable, HexColor};
-use num::Complex;
+use utils::{Arena, QWave, CWave, Waveable, gen_test_pattern};
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
 #[cfg(feature = "wee_alloc")]
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+extern crate web_sys;
 
 #[wasm_bindgen]
 pub struct Canvas {
@@ -21,9 +22,6 @@ pub struct Canvas {
     test: Vec<u32>,
 }
 
-
-
-
 #[wasm_bindgen]
 impl Canvas { 
     #[no_mangle]
@@ -32,33 +30,8 @@ impl Canvas {
         let arena = Arena::new(width, height);
         let image = vec![0xFF000000; (width*height) as usize];
         let wave: QWave = QWave::new(&arena);
+        let test: Vec<u32> = gen_test_pattern(arena.width, arena.height);
         
-        let (min, max) = (i32::MIN >> 1, i32::MAX >> 1); // full range of possible colors
-        // map [0, width] -> [min, max]
-        let sampling_scale = |x: u32| { 
-            let s = (((max as f32 - min as f32)*(x as f32) / width as f32) as i32) + min;
-            return s
-        }; 
-        // map [min, max] -> [-1,1]
-        let scale = |x: i32| { 
-            return ((x as f32) + 0.5)/((i32::MAX >> 1) as f32 - 0.5) 
-        };
-    
-        let mut test: Vec<u32> =  vec![0u32; (width * height) as usize];
-        for x in 0..width {
-            for y in 0..height {
-                let index = (y*width + x) as usize;
-                let complex_x = scale(sampling_scale(x));
-                let arc: f32 = (1.0 - complex_x*complex_x).sqrt();
-                let complex_y = -scale(sampling_scale(y));
-                if -arc <= complex_y && complex_y < arc {
-                    test[index] = Complex { re: sampling_scale(x), im: sampling_scale(y) }.to_rgba();
-                }
-                else { 
-                    test[index] = 0xFF_000000;
-                }
-            }
-        }
         return Canvas {
             arena,
             image,
@@ -67,9 +40,9 @@ impl Canvas {
         }
     }
     #[no_mangle]
-    pub fn step(&mut self,  damping_bit_shift: u8) {
+    pub fn step(&mut self,  damping_bit_shift: u8, norm_only: bool) {
         self.wave.step(&self.arena, damping_bit_shift);
-        self.image = self.wave.render(); 
+        self.image = self.wave.render(norm_only); 
     }
     #[no_mangle]
     pub fn image(&self) -> *const u32 {
@@ -78,6 +51,10 @@ impl Canvas {
     #[no_mangle]
     pub fn test(&self) -> *const u32 {
         self.test.as_ptr()
+    }
+    #[no_mangle]
+    pub fn norm(&self) -> f32 {
+        self.wave.norm
     }
     #[no_mangle]
     pub fn width(&self) -> u32 {
--- crate/src/utils.rs
+++ crate/src/utils.rs
@@ -10,8 +10,17 @@ use num::complex::Complex;
 //     console_error_panic_hook::set_once();
 // }
 
+// A macro to provide `println!(..)`-style syntax for `console.log` logging.
+macro_rules! log {
+    ( $( $t:tt )* ) => {
+        web_sys::console::log_1(&format!( $( $t )* ).into());
+    }
+}
+
 const ALPHA: u32 = 0xFF_00_00_00; 
 const FORCE_DAMPING_BIT_SHIFT: u8 = 4;
+pub const I32_CAP_MIN: i32 = i32::MIN >> 1;
+pub const I32_CAP_MAX: i32 = i32::MAX >> 1;
 
 #[derive(Clone, Copy)]
 pub enum Status {
@@ -31,9 +40,9 @@ impl Arena {
         let h: usize = height as usize;
         let status = (0..w * h).map(|x| {
             if x < w { return Status::Wall };                       // Top
+            if (x + 1) % w == 0 { return Status::Wall };            // Right
             if h * (w - 1) < x && x < h * w { return Status::Wall };// Bottom
             if x % w == 0 { return Status::Wall };                  // Left
-            if (x + 1) % w == 0 { return Status::Wall };            // Right
             return Status::Default;
         }).collect();
 
@@ -42,6 +51,19 @@ impl Arena {
             width,
             status,
         };
+    }
+    pub fn to_xy(&self, idx: usize) -> (i32, i32) {
+        // returns coordinates with origin at center
+        // up = +y, right = +x
+        let w = self.width as usize;
+        let h = self.height as usize;
+        let (x0, y0) = (w / 2, h / 2);
+        let y = y0 - idx / (self.width as usize);
+        let x = (idx % (self.width as usize)) - x0;
+        (x as i32, y as i32)
+    }
+    pub fn to_idx(&self, x: u32, y: u32) -> usize {
+        (y * &self.width) as usize + x as usize
     }
 }
 
@@ -53,12 +75,13 @@ pub struct CWave {
 #[derive(Debug)]
 pub struct QWave {
     psi: Vec<Complex<i32>>,
+    pub norm: f32
 }
 
 pub trait Waveable {
     fn new(arena: &Arena) -> Self;
     fn step(&mut self, arena: &Arena, damping: u8);
-    fn render(&self) -> Vec<u32>;
+    fn render(&self, norm_only: bool) -> Vec<u32>;
 }
 
 impl Waveable for CWave {
@@ -116,44 +139,49 @@ impl Waveable for CWave {
             }
         }
     } 
-    fn render(&self) -> Vec<u32> {
-        return self.u.iter().map(|u: &i32| u.to_rgba()).collect();
+    fn render(&self, _norm_only: bool) -> Vec<u32> {
+        return self.u.iter().map(|u: &i32| u.to_rgba(false)).collect();
     }
 }
-
-// range of i32 is -2^31 to 2^31 - 1
-// i32 in [-0x80000000, 0x7fffffff]
-// half is [-0x40000000, 0x3fffffff]
-//
-// To have full range of i32 datatype, implement mapping:
-// [-0x40000000, 0x3fffffff] -> [-1,1]
-// where the image is the range of the real and imaginary parts of a complex number
-// this is sufficient since normalization entails |z|^2 = 1 => x^2 + y^2 = 1 => x in [-1,1], y in  [-1,1]
-// therefore, we implement the finite difference part with half an i32
-// but we map to [-1,1] implicitly or explicitly when computing the color
-// which will ends up being a u32.
-// So we have: (re in [-0x40000000,0x3fffffff], im in [-0x40000000, 0x3fffffff]) -> ([-1,1],[-1,1]) -> [0xFF000000, 0xFFFFFFFF]  
 
 impl Waveable for QWave {
     fn new(arena: &Arena) -> QWave {
         let w: usize = arena.width as usize;
         let h: usize = arena.height as usize;
-        let mut psi_0: Vec<Complex<i32>> = vec![Complex{re: 0, im: 0}; w * h];
-        psi_0[12] = Complex{re: 0x3fffffff, im: 0}; //delta function
+
+        let gauss = |x: i32, y: i32| -> f32 {
+            let xf = x as f32 / arena.width as f32; 
+            let yf = y as f32 / arena.height as f32;
+            (-xf * xf - yf * yf).exp()
+        };
+
+        let sine = |x: i32, y: i32| -> f32 {
+            let xf = x as f32 / arena.width as f32;
+            let yf = y as f32 / arena.width as f32;
+            xf.sin() * yf.sin()
+        };
+
+        let psi_0: Vec<Complex<i32>> = vec![Complex{re: 0, im: 0}; w * h]
+            .into_iter()
+            .enumerate()
+            .map(|(i, pt)| {
+                let (x, y) = arena.to_xy(i);
+                let amp = gauss(20*x, 5*y)*10.0*sine(90, 90*y);
+                let amp_i32 = (amp * (i32::MAX >> 4) as f32) as i32;
+                return Complex{ re: amp_i32, im: 0 }
+            }).collect();
+        let norm_0 = psi_0.iter().fold(0.0, |acc, &psi| -> f32 {
+            acc + Complex{ re: to_amp(psi.re), im: to_amp(psi.im) }.norm() / ((w * h) as f32)
+        });
         return QWave {
             psi: psi_0,
+            norm: norm_0
         }
     }
     fn step(&mut self, arena: &Arena, _damping: u8) {
 
         // finite difference algorithm
-        // computes psi_next a the point specified by row, col
-        // using a spatial stencil:
-        // [[0,1,0],[1,-4,1],[0,1,0]]
-        // TODO: boundary conditions?
-        // Can implement Neumann boundary conditions by modifiying the
-        // stencil to enable reflections. For example, on the left wall the
-        // stencil becomes: [[0,1,0],[0,-4,2],[0,1,0]]
+
         let w = arena.width as usize;
         let h = arena.height as usize;
 
@@ -189,6 +217,9 @@ impl Waveable for QWave {
                 _ => {}
             }
         }
+        self.norm = self.psi.iter().fold(0.0, |acc, &psi| -> f32 {
+            acc + Complex{ re: to_amp(psi.re), im: to_amp(psi.im) }.norm() / ((w * h) as f32)
+        });
         // let default_neighbors: Complex<i32> = left_neighbor + right_neighbor + top_neighbor + bottom_neighbor;
         // TODO: debug reflections
         // let psi_neighbors: Complex<i32> = match (row, col) {
@@ -203,8 +234,8 @@ impl Waveable for QWave {
         //     (_, _) => default_neighbors
         // };
     }
-    fn render(&self) -> Vec<u32> {
-        return self.psi.iter().map(|x| x.to_rgba()).collect();
+    fn render(&self, norm_only: bool) -> Vec<u32> {
+        return self.psi.iter().map(|x| x.to_rgba(norm_only)).collect();
     }
 }
 
@@ -214,10 +245,10 @@ pub trait Rectified {
 
 impl Rectified for i32 {
     fn apply_cap(self) -> i32 {
-        if self < i32::MIN >> 1 {
-            return i32::MIN >> 1;
-        } else if self > i32::MAX >> 1 {
-            return i32::MAX >> 1;
+        if self < I32_CAP_MIN {
+            return I32_CAP_MIN;
+        } else if self > I32_CAP_MAX {
+            return I32_CAP_MAX;
         } else {
             return self;
         }
@@ -230,12 +261,16 @@ impl Rectified for Complex<i32> {
     }
 }
 
+// Convert i32 to float in [-1, 1]
+pub fn to_amp (x: i32) -> f32 {
+    return ((x as f32) + 0.5)/((I32_CAP_MAX) as f32 - 0.5)
+}
 pub trait HexColor {
-    fn to_rgba(self) -> u32;
+    fn to_rgba(self, norm_only: bool) -> u32;
 }
 
 impl HexColor for i32 {
-    fn to_rgba(self) -> u32 {
+    fn to_rgba(self, norm_only: bool) -> u32 {
         let mut val: i32 = self >> 22;
         if val > 0 {
             let res = val as u32;
@@ -248,19 +283,20 @@ impl HexColor for i32 {
 }
 
 impl HexColor for Complex<i32> {
-    fn to_rgba(self) -> u32 {
-        let scale = |x: i32| { 
-            // maps i32 in [-2^30, 2^30-1] to f32 in [-1,1]
-            return ((x as f32) + 0.5)/((i32::MAX >> 1) as f32 - 0.5) 
-        };
-        let f: Complex<f32> = Complex{ re: scale(self.re), im: scale(self.im) };
+    fn to_rgba(self, norm_only: bool) -> u32 {
+        let f: Complex<f32> = Complex{ re: to_amp(self.re), im: to_amp(self.im) };
+        let value = f.norm(); 
+        if norm_only {
+            let (v, _, __) = hsv_to_rgb(0.0, 1.0, value);
+            return ALPHA | v << 16 | v << 8 | v
+        }
         let hue_in_rads = f.arg();
         let hue_in_degs = hue_in_rads * 180.0 / PI; 
-        let value = f.norm(); 
         let (r,g,b) = hsv_to_rgb(hue_in_degs, 1.0, value);
         return ALPHA | b << 16 | g << 8 | r
     } 
 }
+
 
 pub fn hsv_to_rgb(hue: f32, sat: f32, val: f32) -> (u32, u32, u32) {
     // based on algorithm from https://en.wikipedia.org/wiki/HSL_and_HSV
@@ -285,13 +321,38 @@ pub fn hsv_to_rgb(hue: f32, sat: f32, val: f32) -> (u32, u32, u32) {
     return (r,g,b);
 } 
 
+pub fn gen_test_pattern(width: u32, height: u32) -> Vec<u32> {
+    let (min, max) = (I32_CAP_MIN, I32_CAP_MAX);
+    // map [0, width] -> [min, max]
+    let sampling_scale = |x: u32| { 
+        let s = (((max as f32 - min as f32)*(x as f32) / width as f32) as i32) + min;
+        return s
+    };     
+    let mut test: Vec<u32> =  vec![0u32; (width * height) as usize];
+    for x in 0..width {
+        for y in 0..height {
+            let index = (y*width + x) as usize;
+            let complex_x = to_amp(sampling_scale(x));
+            let arc: f32 = (1.0 - complex_x*complex_x).sqrt();
+            let complex_y = -to_amp(sampling_scale(y));
+            if -arc <= complex_y && complex_y < arc {
+                test[index] = Complex { re: sampling_scale(x), im: sampling_scale(y) }.to_rgba(false);
+            }
+            else { 
+                test[index] = 0xFF_000000;
+            }
+        }
+    }
+    return test
+}
+
 use std::f32::consts::PI; 
 
 #[test]
 pub fn test_to_rgba_red () {
     const RED: u32 = 0xFF_00_00_FF;
     const UNITY: Complex<i32> = Complex {re: 1 << 30, im: 0};
-    let result = UNITY.to_rgba();
+    let result = UNITY.to_rgba(false);
     assert_eq!(result, RED, "\nExpected: 0x{:X}\nActual:   0x{:X}", RED, result);
 }
 
@@ -299,7 +360,7 @@ pub fn test_to_rgba_red () {
 pub fn test_to_rgba_green () {
     const GREEN: u32 = 0xFF_00_FF_00;
     let one_third_turn: Complex<i32> = Complex {re: -(1 << 29), im: (((1 << 29) as f32)*((3.0_f32.sqrt()))) as i32};
-    let result = one_third_turn.to_rgba();
+    let result = one_third_turn.to_rgba(false);
     assert_eq!(result, GREEN, "\nExpected: 0x{:X}\nActual:   0x{:X}", GREEN, result);
 }
 
@@ -307,7 +368,7 @@ pub fn test_to_rgba_green () {
 pub fn test_to_rgba_blue () {
     const BLUE: u32 = 0xFF_FF_00_00;
     let two_third_turn: Complex<i32> = Complex {re: -(1 << 29), im: -(((1 << 29) as f32)*((3.0_f32.sqrt()))) as i32};
-    let result = two_third_turn.to_rgba();
+    let result = two_third_turn.to_rgba(false);
     assert_eq!(result, BLUE, "\nExpected: 0x{:X}\nActual:   0x{:X}", BLUE, result);
 }
 
@@ -344,8 +405,8 @@ pub fn test_black () {
 }
 
 #[test]
-pub fn test_QWave_step () {
-    let test_arena: Arena = Arena::new( 5,  5);
+pub fn test_delta_step () {
+    let test_arena: Arena = Arena::new(5, 5);
     let mut test_wave: QWave = QWave::new(&test_arena);
     let mut result= vec![Complex{re: 0, im: 0}; 25];
     result[7].im =  -test_wave.psi[12].re >> 1;
@@ -354,5 +415,31 @@ pub fn test_QWave_step () {
     result[17].im = -test_wave.psi[12].re >> 1;
     result[12] = Complex{ re: test_wave.psi[12].re, im: 2*test_wave.psi[12].re};
     test_wave.step(&test_arena, 0);
-    assert_eq!(result, test_wave.psi)
+    assert_eq!(result, test_wave.psi, "\n
+    Actual:
+    \n\t\t{}\n{} {} {} \n\t\t{}\n
+    Expected:
+    \n\t\t{}\n{} {} {} \n\t\t{}\n",
+    result[7], 
+    result[11], result[12], result[13], 
+    result[17],
+    test_wave.psi[7], 
+    test_wave.psi[11], test_wave.psi[12], test_wave.psi[13], 
+    test_wave.psi[17])
+}
+
+pub fn test_vline_step () {
+    // TODO: implement
+}
+
+pub fn test_hline_step () {
+    // TODO: implement
+}
+
+pub fn test_left_reflection () {
+    // TODO: implement
+}
+
+pub fn test_right_reflection () {
+    // TODO: implement
 }
